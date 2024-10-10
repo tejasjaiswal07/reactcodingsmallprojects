0c601eba1187122b2625edb1737f2fd5a49237b7
Tejas Jaiswal <20ec112@jssaten.ac.in>
2024-10-10 17:52:15 +0530

    first commit

---
+++ counter-app/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+
+  return(
+    <div>
+      <button onClick = {()=> {setCount(count +1)}}>
+        click: {count}
+
+      </button>
+    </div>
+  )
+
+
+
+}
